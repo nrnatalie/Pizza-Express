2a08656c8f8b5ec59e135b7e47ff9fd062c473d9
Nathalie Russo <nrnatusia7@gmail.com>
2024-11-02 12:36:53 +0200

    add update route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,20 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+// import NextAuth, { AuthOptions } from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
 
-export const authOptions: AuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
-    }),
-  ],
-};
+// export const authOptions: AuthOptions = {
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID || "",
+//       clientSecret: process.env.GITHUB_SECRET || "",
+//     }),
+//   ],
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
+import NextAuth from 'next-auth';
+import { authOptions } from '@/shared/constants/auth-options';
 
 const handler = NextAuth(authOptions);
 
