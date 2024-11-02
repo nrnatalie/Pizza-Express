8602d4673eb25b0333aff3aaa7f37699e5e78c35
Nathalie Russo <nrnatusia7@gmail.com>
2024-11-02 12:08:50 +0200

    add update

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -5,4 +5,4 @@ export { getCartItemDetails } from "./get-cart-item-details";
 export { getCartDetails } from "./get-cart-details";
 export { calcCartItemTotalPrice } from "./calc-cart-item-total-price";
 export { sendEmail } from "./send-email";
-export { createPayment } from "./creat-payment";
+
